864b22f9024e46090f0a4307851eb8fb44c92eda
John Munn <johnmunn5@gmail.com>
2025-11-13 11:52:42 -0400

    update challenges

---
+++ leetcode/2025-11-08/Index.js
@@ -0,0 +1,25 @@
+function findMaxForm(strs, m, n) {
+    const dp = Array.from({ length: m + 1 }, () =>
+        new Array(n + 1).fill(0)
+    );
+    for (const s of strs) {
+        let zeros = 0;
+        let ones = 0;
+        
+        for (const ch of s) {
+            if (ch === '0') zeros++;
+            else if (ch === '1') ones++;
+        }
+        
+        for (let i = m; i >= zeros; i--) {
+            for (let j = n; j >= ones; j--) {
+                dp[i][j] = Math.max(
+                    dp[i][j],
+                    dp[i - zeros][j - ones] + 1
+                );
+            }
+        }
+    }
+    
+    return dp[m][n];
+}
