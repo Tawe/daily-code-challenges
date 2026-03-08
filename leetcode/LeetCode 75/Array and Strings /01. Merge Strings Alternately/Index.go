c8a87bfadb7981a5280eb3d72a9ea677934ecf7f
John Munn <munn.jon@gmail.com>
2026-03-07 22:55:22 -0400

    new challenge

---
+++ leetcode/LeetCode 75/Array and Strings /01. Merge Strings Alternately/Index.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func mergeAlternately(word1 string, word2 string) string {
+  
+}
+
+func main() {
+	fmt.Println(mergeAlternately("abc", "pqr"))
+	fmt.Println(mergeAlternately("ab", "pqrs"))
+	fmt.Println(mergeAlternately("abcd", "pq"))
+}
