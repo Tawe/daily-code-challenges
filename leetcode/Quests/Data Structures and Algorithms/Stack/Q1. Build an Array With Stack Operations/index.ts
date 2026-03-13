dcd3593f88c51cc4531b768b62f9139829eb7e17
John Munn <munn.jon@gmail.com>
2026-03-13 15:13:17 -0300

    Quest

---
+++ leetcode/Quests/Data Structures and Algorithms/Stack/Q1. Build an Array With Stack Operations/index.ts
@@ -0,0 +1,13 @@
+function buildArray(target: number[], n: number): string[] {
+    const stack: string[] = [];
+    for (let i = 1; i <= target[target.length - 1]; i++) {
+        stack.push("Push");
+        if (target.indexOf(i) === -1) {
+            stack.push("Pop");
+        } 
+    }
+    return stack.filter(item => item !== "Pop");
+};
+
+const x = buildArray([1,3], 3);
+x;
