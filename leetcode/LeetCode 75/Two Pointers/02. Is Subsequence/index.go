5db2d5ad6820ef1f448d4e1f4b75d5393552a9b9
John Munn <munn.jon@gmail.com>
2026-03-16 11:38:08 -0300

    Challenge

---
+++ leetcode/LeetCode 75/Two Pointers/02. Is Subsequence/index.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isSubsequence(s string, t string) bool {
+	i := 0
+
+	for j := 0; j < len(t) && i < len(s); j++ {
+		if s[i] == t[j] {
+			i++
+		}
+	}
+
+	return i == len(s)
+}
+
+func main() {
+	fmt.Println(isSubsequence("abc", "ahbgdc"))
+	fmt.Println(isSubsequence("axc", "ahbgdc"))
+}
