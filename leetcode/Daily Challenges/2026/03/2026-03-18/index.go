46c74a189d86724ab58e9efdd65a64054994061c
John Munn <munn.jon@gmail.com>
2026-03-18 10:34:09 -0300

    Readding challenge

---
+++ leetcode/Daily Challenges/2026/03/2026-03-18/index.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func countSubmatrices(grid [][]int, k int) int {
+    
+}
+
+func main() {
+	fmt.Println(countSubmatrices([][]int{{7,6,3},{6,6,1}}, 18))
+	fmt.Println(countSubmatrices([][]int{{7,2,9},{1,5,0},{2,6,6}}, 20))
+}
--- leetcode/LeetCode 75/Array and Strings /01. Merge Strings Alternately/Index.go
+++ leetcode/LeetCode 75/Array and Strings /01. Merge Strings Alternately/Index.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func mergeAlternately(word1 string, word2 string) string {
-  
+	result := ""
+	for i := 0; i < len(word1) || i < len(word2); i++ {
+		if i < len(word1) {
+			result += string(word1[i])
+		}
+		if i < len(word2) {
+			result += string(word2[i])
+		}
+	}
+	return result
 }
 
 func main() {
