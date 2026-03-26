ce85bcf5290d2d4c274f94df19393086919cafbc
John Munn <munn.jon@gmail.com>
2026-03-26 09:35:11 -0300

    Challenges

---
+++ leetcode/LeetCode 75/Hash Map Sets/02. Unique Number of Occurrences/index.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func uniqueOccurrences(arr []int) bool {
+    freq := make(map[int]int)
+	for _, v := range arr {
+		freq[v]++
+	}
+
+	occurrences := make(map[int]bool)
+	for _, v := range freq {
+		if occurrences[v] {
+			return false
+		}
+		occurrences[v] = true
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(uniqueOccurrences([]int{1,2,2,1,1,3}))
+	fmt.Println(uniqueOccurrences([]int{1,2}))
+	fmt.Println(uniqueOccurrences([]int{-3,0,1,-3,1,1,1,-3,10,0}))
+}
