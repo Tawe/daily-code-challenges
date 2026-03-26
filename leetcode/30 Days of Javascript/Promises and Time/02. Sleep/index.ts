ce009b8e2e174cba92bc0f70c386b7aaa8a18561
John Munn <munn.jon@gmail.com>
2026-03-26 09:39:06 -0300

    Challenge

---
+++ leetcode/30 Days of Javascript/Promises and Time/02. Sleep/index.ts
@@ -0,0 +1,8 @@
+async function sleep(millis: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, millis))
+}
+
+const t = Date.now();
+sleep(100).then(() => {
+    console.log(Date.now() - t);
+});
