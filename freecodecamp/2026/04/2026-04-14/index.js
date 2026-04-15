a48df120d0da0f906ddf6e17fbbe01cc243f7031
John Munn <munn.jon@gmail.com>
2026-04-15 10:11:28 -0300

    Challenge

--- freecodecamp/2026/04/2026-04-14/index.js
+++ freecodecamp/2026/04/2026-04-14/index.js
@@ -1,4 +1,23 @@
 function getLastLetter(str) {
+    let lastLetter = "";
+    let bestLower = "";
 
-    return str;
+    for (let i = 0; i < str.length; i++) {
+        const ch = str[i];
+        const lower = ch.toLowerCase();
+        const isLetter = lower >= "a" && lower <= "z";
+
+        if (!isLetter) {
+            continue;
+        }
+
+        if (bestLower === "" || lower > bestLower) {
+            bestLower = lower;
+            lastLetter = ch;
+        }
+    }
+
+    return lastLetter;
 }
+
+console.log(getLastLetter("Hello World"));
